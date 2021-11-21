ccbbbcc91b80f5d824616db5063cb8b9570cf591
Akkrapol Kanpong <aukkrapolarm@gmail.com>
2021-11-21 20:47:37 +0700

    ✌️ Update Register Front/Back

--- client/pages/register.js
+++ client/pages/register.js
@@ -1,13 +1,34 @@
 import { useState } from "react";
+import axios from "axios";
+import { toast } from "react-toastify";
+import { SyncOutlined } from "@ant-design/icons";
 
 const Register = () => {
     const [name, setName] = useState("");
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const [loading, setLoading] = useState(false);
 
-    const handleSubmit = (e) => {
+    const handleSubmit = async (e) => {
         e.preventDefault();
-        console.table({ name, email, password });
+        // console.table({ name, email, password });
+        try {
+            setLoading(true);
+            const { data } = await axios.post(
+                `${process.env.NEXT_PUBLIC_API}/register`,
+                {
+                    name,
+                    email,
+                    password,
+                }
+            );
+            // console.log("REGISTER RESPONSE", data);
+            toast("Registration successful. Please login.");
+            setLoading(false);
+        } catch (err) {
+            toast(err.response.data);
+            setLoading(false);
+        }
     };
 
     return (
@@ -18,36 +39,46 @@ const Register = () => {
 
             <div className="container col-md-4 offset-md-4 pb-5">
                 <form onSubmit={handleSubmit}>
-                    <input
-                        type="text"
-                        className="form-control mb-4 p-4"
-                        value={name}
-                        onChange={(e) => setName(e.target.value)}
-                        placeholder="Enter name"
-                        required
-                    />
+                    <div className="row">
+                        <input
+                            type="text"
+                            className="form-control mb-4 p-4"
+                            value={name}
+                            onChange={(e) => setName(e.target.value)}
+                            placeholder="Enter name"
+                            required
+                        />
+                    </div>
+                    <div className="row">
+                        <input
+                            type="email"
+                            className="form-control mb-4 p-4"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                            placeholder="Enter email"
+                            required
+                        />
+                    </div>
 
-                    <input
-                        type="email"
-                        className="form-control mb-4 p-4"
-                        value={email}
-                        onChange={(e) => setEmail(e.target.value)}
-                        placeholder="Enter email"
-                        required
-                    />
-
-                    <input
-                        type="password"
-                        className="form-control mb-4 p-4"
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                        placeholder="Enter password"
-                        required
-                    />
-
-                    <button type="submit" className="btn btn-block btn-primary">
-                        Submit
-                    </button>
+                    <div className="row">
+                        <input
+                            type="password"
+                            className="form-control mb-4 p-4"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                            placeholder="Enter password"
+                            required
+                        />
+                    </div>
+                    <div className="row">
+                        <button
+                            type="submit"
+                            className="btn btn-block btn-primary"
+                            disabled={!name || !email || !password || loading}
+                        >
+                            {loading ? <SyncOutlined spin /> : "Submit"}
+                        </button>
+                    </div>
                 </form>
             </div>
         </>
