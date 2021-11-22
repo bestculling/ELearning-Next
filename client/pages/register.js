43da1536d1b71c4c9f066cac948b5edfc939101a
Akkrapol Kanpong <aukkrapolarm@gmail.com>
2021-11-23 01:56:21 +0700

    ✌️ Update Login Front

--- client/pages/register.js
+++ client/pages/register.js
@@ -2,6 +2,7 @@ import { useState } from "react";
 import axios from "axios";
 import { toast } from "react-toastify";
 import { SyncOutlined } from "@ant-design/icons";
+import Link from "next/link";
 
 const Register = () => {
     const [name, setName] = useState("");
@@ -80,6 +81,12 @@ const Register = () => {
                         </button>
                     </div>
                 </form>
+                <p className="text-center p-3">
+                    Already registered?{" "}
+                    <Link href="/login">
+                        <a>Login</a>
+                    </Link>
+                </p>
             </div>
         </>
     );
